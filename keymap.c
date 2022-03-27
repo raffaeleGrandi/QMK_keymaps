0344d731406905bff7f3cae8826fdf1337c01c05
RGrandi <raffaele.grandi@gmail.com>
2022-03-27 10:13:02 +0200

    Added caps_lock

--- keymap.c
+++ keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |Qwerty|  N/O |BrigDw|BrigUp| N/O  |  N/O |  N/O | Pad4 | Pad5 | Pad6 | Pad/ |PadENT|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  N/O |  N/O | Vol- | Vol+ | N/O  |  N/O |NUM LK| Pad1 | Pad2 | Pad3 | Pad- | Pad, |
+ * |CAPSLK|  N/O | Vol- | Vol+ | N/O  |  N/O |NUM LK| Pad1 | Pad2 | Pad3 | Pad- | Pad, |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |  N/O |      | Mute |  N/O |      | PrintScreen |      | Pad0 | Pad. | Pad+ | Pad= |
  * `-----------------------------------------------------------------------------------'
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC, \
   DEBUG,   KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,    KC_CALC, KC_P7,   KC_P8,   KC_P9,   KC_PAST, KC_DEL,  \
   DF(0),   KC_NO,   KC_BRID, KC_BRIU, KC_NO,   KC_NO,    KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PSLS, KC_PENT, \
-  KC_NO,   KC_NO,   KC_VOLD, KC_VOLU, KC_NO,   KC_NO,    KC_NLCK, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PCMM, \
+  KC_CAPS, KC_NO,   KC_VOLD, KC_VOLU, KC_NO,   KC_NO,    KC_NLCK, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PCMM, \
   KC_NO,   _______, KC_MUTE, KC_NO,   _______,     KC_PSCR,       _______, KC_P0,   KC_PDOT, KC_PPLS, KC_PEQL  \
 ),
 
